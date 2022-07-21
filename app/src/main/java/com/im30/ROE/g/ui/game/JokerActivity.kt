ed33b1583cc0daaf0978103a85d9b78566c6708e
SlavaAndroid <slava150185@gmail.com>
2022-07-21 17:18:55 +0300

    second commit

---
+++ app/src/main/java/com/im30/ROE/g/ui/game/JokerActivity.kt
@@ -0,0 +1,17 @@
+package com.im30.ROE.g.ui.game
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.im30.ROE.g.databinding.ActivityJokerBinding
+
+
+class JokerActivity: AppCompatActivity()  {
+
+    private lateinit var binding: ActivityJokerBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityJokerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
